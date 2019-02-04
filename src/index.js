1cf33f825a42eb7511f3123122d9eb6fc60372e0
sajeeva <sa.hasith@gmail.com>
2019-02-04 19:00:54 +0530

    add navigation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 // import Counters from './components/counters';
-import Counters from './components/counters';
+// import Counters from './components/counters';
 // import Counter from './components/counter';
 // import Counter from './components/counterDemo2';
 // import Counter from './components/counterDemo3';
 // import Counter from './components/counterDemo4';
 
-ReactDOM.render(<Counters />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
